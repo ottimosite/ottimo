a455b7b7fa53f9a2c3e6d3a7c5b791396c051586
Adrian Badiu <adrian.badiu@mail.com>
2021-04-17 17:08:05 +0100

    1.3.0 - imports tw directly

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,12 @@
 import React from 'react'
 
 import Layout from '../components/Layout'
-import Seo from '../components/Seo'
+// import Seo from '../components/Seo'
 import Hero from '../components/Patterns/Hero'
 
 const IndexPage = () => (
 	<Layout className="styles-center justify-center">
-		<Seo title="Home" />
+		{/* <Seo title="Home" /> */}
 		<Hero />
 	</Layout>
 )
