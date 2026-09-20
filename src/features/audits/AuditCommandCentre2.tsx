19df4aabc08ab788a5c4ca8a68077f3ab75b463b
Adrian Badiu <adrian.badiu@mail.com>
2026-09-20 15:21:23 +0100

    fix: remove unused command centre import

--- src/features/audits/AuditCommandCentre2.tsx
+++ src/features/audits/AuditCommandCentre2.tsx
@@ -1,4 +1,3 @@
-import { Link } from 'react-router-dom'
 import type { Audit, OptimizationAction } from '../../types/domain'
 import { blockingDependencies } from '../../audit-engine/action-dependencies'
 
